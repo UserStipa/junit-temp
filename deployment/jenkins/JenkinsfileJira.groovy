06fec6c2e2d86c455e16db43acb958555a5e816d
UserStipa <945.kovdim@gmail.com>
2025-07-21 15:47:37 +0300

    Add mirror

--- deployment/jenkins/JenkinsfileJira.groovy
+++ deployment/jenkins/JenkinsfileJira.groovy
@@ -5,6 +5,11 @@ pipeline {
         }
     }
     stages {
+        stage('Git') {
+            steps {
+                git branch: 'master', url: 'https://github.com/UserStipa/junit-temp.git'
+            }
+        }
         stage('Run Tests') {
             steps {
                 withMaven(maven: 'Maven 3.6.1') {
